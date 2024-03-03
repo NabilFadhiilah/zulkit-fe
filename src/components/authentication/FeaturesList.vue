aebf18789f290358722389bfcb8cebce558cb8cc
Nabil Fadhiilah <nabil.10518027@mahasiswa.unikom.ac.id>
2024-03-03 16:43:28 +0700

    Sesi 10

---
+++ src/components/authentication/FeaturesList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import RegisterForm from '@/components/authentication/RegisterForm.vue';
+import Customizable from '@/components/authentication/features/Customizable.vue';
+import Documentation from '@/components/authentication/features/Documentation.vue';
+import Fortune from '@/components/authentication/features/Fortune.vue';
+</script>
+
+<template>
+    <Customizable />
+    <Documentation />
+    <Fortune />
+</template>
